43450c18a2d2304ed6db8250facdef64abd3baa4
Latifa_Simbini <simbinelatifa@gmail.com>
2019-08-05 13:17:02 +0200

    Update visitor.js

--- visitor.js
+++ visitor.js
@@ -18,20 +18,24 @@ class Visitor {
   
    save() {
      this.Count();
-    var save = fs.writeFile("visitor" + count + ".json",`Fullname: ${this.name} ${this.surname} \n Date: ${this.date} \n Time: ${this.time}\n assistant: ${this.assistant}.`);
-   
+    fs.writeFile("visitor" + count + ".json",`Fullname: ${this.name} ${this.surname} \n Date: ${this.date} \n Time: ${this.time}\n assistant: ${this.assistant}.`, function (err) {
+        if(err){
+            throw err;
+        }
+    });
    }
    load(i) {
-       if(i < 1) {
-           console.log("File does not  exist");
-       }
-       var data =  fs.readFile("visitor" + i);
-       console.log(data);
-   }
-}
-var alice = new Visitor( "alice", "mere", 12/07/2019,1, "josh");
+    
+       fs.readFile("./visitor" + i+".json", "utf8",(err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
+}}
+var alice = new Visitor( "alice", "mere", 12/07/2019,6, "josh");
 var bob = new Visitor( "bob", "smith", 2/1/2019, 7, "sally");
 var charlie = new Visitor( "chralie", "puth", 6/2/2019, 8, "peter");
+
 alice.save();
 bob.save();
 charlie.save();
+alice.load(1);
